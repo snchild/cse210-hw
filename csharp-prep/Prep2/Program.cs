2de5bd3a9e6f0050d633f93ea170dfd81c311cd4
Sierra Child <sierrachild@yahoo.com>
2023-01-05 15:55:41 -0700

    completed prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percetnage? ");
+        string gradeAnswer = Console.ReadLine();
+        int grade = int.Parse(gradeAnswer);
+        
+        string letter;
+
+        if(grade >= 90)
+        {
+            letter = "A";
+        }
+        else if(grade >= 80)
+        {
+            letter = "B";
+        }
+        else if(grade >= 70)
+        {
+            letter = "C";
+        }
+        else if(grade >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}.");
+
+        if(grade >= 70)
+        {
+            Console.WriteLine("Congradulations on passing!");
+        }
+        else{
+            Console.WriteLine("Try better next time.");
+        }
     }
 }
