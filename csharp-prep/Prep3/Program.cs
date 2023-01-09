ea0596e885180085a073f10994d2f209755be3ec
Sierra Child <sierrachild@yahoo.com>
2023-01-09 11:00:46 -0700

    completed prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1, 101); 
+        
+        bool isCorrect = false;
+        
+        do{
+            //asking for a guess
+            Console.WriteLine("What is your guess? ");
+            string guess = Console.ReadLine();
+            int guessNumber = int.Parse(guess);
+
+            //checking the guess
+            if (guessNumber == randomNumber)
+            {
+                Console.WriteLine("You got it!");
+                isCorrect = true;
+            }
+            else if(guessNumber > randomNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else{
+                Console.WriteLine("Higher");
+            }
+        }while(isCorrect == false);
+
     }
 }
