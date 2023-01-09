044e49c2344b3610f70cc4b7ad1ed0e8bf1a8347
Sierra Child <sierrachild@yahoo.com>
2023-01-09 15:03:02 -0700

    completed prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,33 @@
 using System;
-
+//using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+        //declaring the list and inputedNumber variable
+        List<int> numbers = new List<int>();
+        int inputedNumber = 1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do{
+            Console.Write("Enter number: ");
+            string userInput = Console.ReadLine();
+            inputedNumber = int.Parse(userInput);
+            
+            //this prevents appending the inputed 0
+            if (inputedNumber != 0){
+                numbers.Add(inputedNumber);
+            }
+        }while(inputedNumber != 0);
+        
+        //print sum, avg, max
+        double sumOfNumbers = numbers.Sum();
+        Console.WriteLine($"The sum is: {sumOfNumbers}");
+        double meanOfNumbers = numbers.Average();
+        Console.WriteLine($"The average is: {meanOfNumbers}");
+        double maxOfNumbers = numbers.Max();
+        Console.WriteLine($"The largest number is: {maxOfNumbers}");
+
     }
 }
