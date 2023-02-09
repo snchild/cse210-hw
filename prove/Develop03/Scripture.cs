26ba82486863743c62429ba8860450e848cea1c7
Sierra Child <sierrachild@yahoo.com>
2023-02-09 15:00:43 -0700

    completed develop 3

--- prove/Develop03/Scripture.cs
+++ prove/Develop03/Scripture.cs
@@ -1,7 +1,6 @@
 public class Scripture
 {
     private string _reference = "Proverbs 3:5-6";
-    //public List<string> _verse = new List<string>() {"That","whosoever","believeth","in","him","should","not","perish","but","have","eternal","life."};
     public List<string> _verse = new List<string>() {"Trust", "in", "the", "Lord","with","all","thine","heart;","and","lean","not","unto","thine","own","understanding.","\nIn","all","thy","ways","acknowledge","him,","and","he","shall","direct","thy","paths."};
     public int GetVerseLength()
     {
@@ -21,20 +20,20 @@ public class Scripture
         Console.WriteLine(" "); //makes it so the next words are on a different line
 
     }
-    private int GetCharacterLength(int index) //wip
+    private int GetCharacterLength(int index) 
     {
         Console.WriteLine("running GetCharacterLength");
         int characterLength = _verse[index].Length;
         return characterLength;
     }
-    public void ReplaceWord(List<int> words) //wip
+    public void ReplaceWord(List<int> words) 
     {
         Console.WriteLine("running ReplaceWord");
         
         //do loop through all of that list
         for(int i = 0; i < words.Count; i++){
             int length = GetCharacterLength(words[i]); //get the character length
-            //at that index, replace the word with _*numb of characters
+            //at that index, replace the word with _*number of characters
             string replacement = new String('_',length);
             _verse[words[i]] = replacement;
         }
